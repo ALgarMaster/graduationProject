69fb34006c67daa87666bb09573c6af6b4e014ac
ALgarMaster <133888451+ALgarMaster@users.noreply.github.com>
2024-11-07 22:37:01 +0300

    Fullest function colorPaletteInlineKeyboard colors

--- src/main/java/com/example/graduationProject/service/CustomInlineKeyboardMarkup.java
+++ src/main/java/com/example/graduationProject/service/CustomInlineKeyboardMarkup.java
@@ -23,8 +23,8 @@ public class CustomInlineKeyboardMarkup extends InlineKeyboardMarkup {
         InlineKeyboardButton contactSellerButton = createInlineKeyboardButtonSetTextAndSetCallBack("Cвязь с продавцом", "/contactseller");
         InlineKeyboardButton exitButton = createInlineKeyboardButtonSetTextAndSetCallBack("Отменити заказ", "/exit");
 
-        addRowsInLine(backButton, contactSellerButton, exitButton);
-
+        addRowsInLine(backButton,  exitButton);
+        addRowsInLine(contactSellerButton);
         inlineKeyboard.setKeyboard(rowsInLine);
 
         return inlineKeyboard;
@@ -44,22 +44,55 @@ public class CustomInlineKeyboardMarkup extends InlineKeyboardMarkup {
 
     public InlineKeyboardMarkup sizeInlineKeyboard(InlineKeyboardMarkup inlineKeyboard){
 
+        InlineKeyboardButton smallButton = createInlineKeyboardButtonSetTextAndSetCallBack("Маленький", "/small");
+        InlineKeyboardButton averageButton = createInlineKeyboardButtonSetTextAndSetCallBack("Средний", "/average");
+        InlineKeyboardButton bigButton = createInlineKeyboardButtonSetTextAndSetCallBack("Большой", "/big");
 
+        addRowsInLine(smallButton, averageButton, bigButton);
+        inlineKeyboard = addDefaultLine(inlineKeyboard);
 
         return inlineKeyboard;
     }
 
     public InlineKeyboardMarkup genderInlineKeyboard(InlineKeyboardMarkup inlineKeyboard){
 
+        InlineKeyboardButton himButton = createInlineKeyboardButtonSetTextAndSetCallBack("Для него", "/for_him");
+        InlineKeyboardButton herButton = createInlineKeyboardButtonSetTextAndSetCallBack("Для неё", "/for_her");
+        InlineKeyboardButton multipurposeButton = createInlineKeyboardButtonSetTextAndSetCallBack("Универсальный", "/for_multipurpose");
+
+        addRowsInLine(himButton, herButton, multipurposeButton);
+        inlineKeyboard = addDefaultLine(inlineKeyboard);
+
         return inlineKeyboard;
     }
 
     public InlineKeyboardMarkup subjectInlineKeyboard(InlineKeyboardMarkup inlineKeyboard){
 
+
+
         return inlineKeyboard;
     }
 
     public InlineKeyboardMarkup colorPaletteInlineKeyboard(InlineKeyboardMarkup inlineKeyboard){
+        InlineKeyboardButton red = createInlineKeyboardButtonSetTextAndSetCallBack("Красный", "/red");
+        InlineKeyboardButton yellow = createInlineKeyboardButtonSetTextAndSetCallBack("Желтый", "/yellow");
+        InlineKeyboardButton pink = createInlineKeyboardButtonSetTextAndSetCallBack("Розовый", "/pink");
+        InlineKeyboardButton green = createInlineKeyboardButtonSetTextAndSetCallBack("Зеленый", "/green");
+        InlineKeyboardButton sky = createInlineKeyboardButtonSetTextAndSetCallBack("Голубой", "/sky");
+        InlineKeyboardButton blue = createInlineKeyboardButtonSetTextAndSetCallBack("Синий", "/blue");
+        InlineKeyboardButton brown = createInlineKeyboardButtonSetTextAndSetCallBack("Коричневый", "/brown");
+        InlineKeyboardButton violet = createInlineKeyboardButtonSetTextAndSetCallBack("Фиолетовый", "/violet");
+        InlineKeyboardButton darkGreen = createInlineKeyboardButtonSetTextAndSetCallBack("Темно зеленый", "/darkGreen");
+        InlineKeyboardButton purple = createInlineKeyboardButtonSetTextAndSetCallBack("Лиловый", "/purple");
+        InlineKeyboardButton cream = createInlineKeyboardButtonSetTextAndSetCallBack("Кремовый", "/cream");
+
+        addRowsInLine(red, yellow, pink);
+        addRowsInLine(green,sky, blue);
+        addRowsInLine(brown, violet );
+        addRowsInLine(darkGreen);
+        addRowsInLine(purple, cream);
+
+        inlineKeyboard = addDefaultLine(inlineKeyboard);
 
         return inlineKeyboard;
     }
